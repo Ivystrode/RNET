420f2785687b110dbcffa7729ec6e0f1dc663b8e
Ivystrode <benjamin200@hotmail.co.uk>
2021-05-21 13:34:29 +0100

    sync

--- unit_con/actions/cpu_con.py
+++ unit_con/actions/cpu_con.py
@@ -4,7 +4,7 @@ import subprocess
 label = "[" + socket.gethostname() + "]"
 
 def reboot():
-    reboot_command = "/usr/bin/sudo /sbin/reboot -r now"
+    reboot_command = "/usr/bin/sudo /sbin/reboot now"
     process = subprocess.Popen(reboot_command.split(), stdout=subprocess.PIPE)
     output=process.communicate()[0]
     print(f"{label} {output}")
