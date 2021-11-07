a8a6adbc7691cca92aaebb55bfeb2fbb8b33e082
Ivystrode <benjamin200@hotmail.co.uk>
2021-11-07 22:45:57 +0000

    vid stream using flask server on unit

--- unit_con/control/cam_con.py
+++ unit_con/control/cam_con.py
@@ -10,7 +10,7 @@ import os
 import time
 from unit_id import unit_details
 
-import vid_stream
+from unit_con import vid_stream
 
 from tqdm import tqdm
 
@@ -93,8 +93,30 @@ def send_photo(hub_addr, file, file_description):
     
 
 # ==========Live video streaming==========
+app = Flask(__name__) 
+vc = cv2.VideoCapture(0) 
+@app.route('/') 
+def index(): 
+   """Video streaming .""" 
+   return render_template('index.html') 
+def gen(): 
+   """Video streaming generator function.""" 
+   while True: 
+       rval, frame = vc.read() 
+       cv2.imwrite('pic.jpg', frame) 
+       yield (b'--frame\r\n' 
+              b'Content-Type: image/jpeg\r\n\r\n' + open('pic.jpg', 'rb').read() + b'\r\n') 
+@app.route('/video_feed') 
+def video_feed(): 
+   """Video streaming route. Put this in the src attribute of an img tag.""" 
+   return Response(gen(), 
+                   mimetype='multipart/x-mixed-replace; boundary=frame') 
+
 def stream_video():
-    vid_stream.stream()
+	app.run(host='0.0.0.0', debug=True, threaded=True) 
+
+# def stream_video():
+    
 
     # app=Flask(__name__)
     # camera=PiCamera()
