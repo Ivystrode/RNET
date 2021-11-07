279b05ccbe7a1a4d2ad1a538f574d70a831f47b8
Ivystrode <benjamin200@hotmail.co.uk>
2021-11-07 23:37:58 +0000

    fufckckdjksfcd

--- unit_con/control/vid_stream.py
+++ unit_con/control/vid_stream.py
@@ -1,30 +1,31 @@
-# import required modules
-from flask import Flask, render_template, Response 
-import picamera 
-import cv2
+from flask import Flask, render_template, Response, request
+import time
+import picamera
+import os
 
-app = Flask(__name__, template_folder='/home/pi/Code/rnet/rnet_testing/unit_con/control/templates') 
-vc = picamera.PiCamera()
-@app.route('/') 
-def index(): 
-   """Video streaming .""" 
-   print("index")
-   return render_template("index.html") 
-def gen(): 
-   """Video streaming generator function.""" 
-   print("gen")
-   while True: 
-       rval, frame = vc.read() 
-       cv2.imwrite('pic.jpg', frame) 
-       yield (b'--frame\r\n' 
-              b'Content-Type: image/jpeg\r\n\r\n' + open('pic.jpg', 'rb').read() + b'\r\n') 
-@app.route('/video_feed') 
-def video_feed(): 
-   """Video streaming route. Put this in the src attribute of an img tag.""" 
-   print("feed")
-   return Response(gen(vc), 
-                   mimetype='multipart/x-mixed-replace; boundary=frame') 
+app = Flask(__name__)
+#app = Flask(__name__, template_folder='/var/www/html/templates')
 
-def run():
-    print("whoo")
-    app.run(host='0.0.0.0', port=5555, debug=True, threaded=True)
+@app.route('/', methods=['GET', 'POST'])
+def move():
+    result = ""
+    if request.method == 'POST':
+        
+        return render_template('index.html', res_str=result)
+                        
+    return render_template('index.html')
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(picamera.PiCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, threaded=True)
