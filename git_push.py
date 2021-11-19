593cd45850a53ee4543d784d59c521c862d70f58
Ivystrode <benjamin200@hotmail.co.uk>
2021-11-19 16:26:51 +0000

    update git push script

--- git_push.py
+++ git_push.py
@@ -5,12 +5,12 @@ def push_to_repo(commitmessage):
     a = subprocess.run(['git','add','.'])
     if a.returncode == 0:
         print("added")
-    b = subprocess.run(['git','commit','-m',f'{commitmessage}'])
-    if b.returncode == 0:
-        print("committed")
-    r = subprocess.run(['git','push','origin','master'])
-    if r.returncode == 0:
-        print("pushed")
+        b = subprocess.run(['git','commit','-m',f'{commitmessage}'])
+        if b.returncode == 0:
+            print("committed")
+            r = subprocess.run(['git','push','origin','master'])
+            if r.returncode == 0:
+                print("pushed")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
