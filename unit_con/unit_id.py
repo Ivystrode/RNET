78916d61b20292727945176684411a1969fc8376
Ivystrode <benjamin200@hotmail.co.uk>
2021-11-20 03:30:48 +0000

    unit id now based on hash of name

--- unit_con/unit_id.py
+++ unit_con/unit_id.py
@@ -6,7 +6,7 @@ import socket, random
 
 unit_details = {
     "unit_name": socket.gethostname().upper(),
-    "unit_id": random.randint(1, 10000),
+    "unit_id": hash(socket.gethostname()),
     "type": "prototype",
     "description": "Test unit for development",
     "hub_address": "192.168.1.79", # MEDEND NETWORK
