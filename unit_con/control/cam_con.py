e3b354fa80d720812230439578898eb82220c102
Ivystrode <benjamin200@hotmail.co.uk>
2021-11-20 04:56:00 +0000

    cam_con update

--- unit_con/control/cam_con.py
+++ unit_con/control/cam_con.py
@@ -281,7 +281,7 @@ def im_recog():
                             # cv2.putText(frame, f"{labels[ClassInd-1].capitalize()}: {round(float(conf*100), 1)}%",(boxes[0], boxes[1]-10), font, fontScale=font_scale, color=(0,255,0), thickness=2)
 
                             if not detection:
-                                cv2.imwrite(f"detection.jpg", frame)
+                                cv2.imwrite(f"detection.jpg", image)
                                 detection = True
                                 print(f"{labels[ClassInd-1]} detected, dimensions: {boxes}, confidence: {round(float(conf*100), 1)}%")
                                 send_photo(unit_details['hub_address'], "detection.jpg", f"{labels[ClassInd-1].capitalize()} detected: {round(float(conf*100), 1)}% confidence")
