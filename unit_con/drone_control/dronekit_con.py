6be1b7db2f346c32b859c30f861aefb5b251523f
Ivystrode <benjamin200@hotmail.co.uk>
2021-11-21 00:49:08 +0000

    hangup in drone_control...had it setup for sim testing only

--- unit_con/drone_control/dronekit_con.py
+++ unit_con/drone_control/dronekit_con.py
@@ -12,26 +12,7 @@ import unit_dbcontrol
 from drone_control import signaller
 from unit_id import unit_details
 
-# connecting to local vehicle simulated
-# can this work if we use VPN/local network IP addresses??
-vehicle = connect("127.0.0.1:14550", wait_ready=True)
 
-HUB_ADDRESS = unit_details['hub_address']
-SIGNAL_PORT = 7501
-
-#connecting over serial device (for rpi connected to the vehicle via serial port, use com14 for windows serial AND telem radio)
-# vehicle = connect("/dev/ttyAMA0", wait_ready=True, baud=57600)
-
-"""
-Connect to multiple vehicles by calling connect() for each vehicle
-Each vehicle will need to be a separate class?
-"""
-print("Connected")
-print(f"Global location: {vehicle.location.global_frame}")
-print(f"GPS: {vehicle.gps_0}")
-print(f"Can be armed: {vehicle.is_armable}")
-print(f"Mode: {vehicle.mode.name}")
-print(f"Armed: {vehicle.armed}")
 
 
 def command_subrouter(command):
@@ -96,6 +77,26 @@ def travel(dest, groundspeed: float):
     unit_dbcontrol.update_status("Travelling")
 
 if __name__ == '__main__':
+    # connecting to local vehicle simulated
+    # can this work if we use VPN/local network IP addresses??
+    vehicle = connect("127.0.0.1:14550", wait_ready=True)
+
+    HUB_ADDRESS = unit_details['hub_address']
+    SIGNAL_PORT = 7501
+
+    #connecting over serial device (for rpi connected to the vehicle via serial port, use com14 for windows serial AND telem radio)
+    # vehicle = connect("/dev/ttyAMA0", wait_ready=True, baud=57600)
+
+    """
+    Connect to multiple vehicles by calling connect() for each vehicle
+    Each vehicle will need to be a separate class?
+    """
+    print("Connected")
+    print(f"Global location: {vehicle.location.global_frame}")
+    print(f"GPS: {vehicle.gps_0}")
+    print(f"Can be armed: {vehicle.is_armable}")
+    print(f"Mode: {vehicle.mode.name}")
+    print(f"Armed: {vehicle.armed}")
     if not vehicle.armed:
         launch(10)
     travel([51.1415097, -4.2534511, 30], 15.0)
